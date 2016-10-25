fbbfb8edb94136c15577d7b98755581b6ba6bbb6
gavra1870 <gavra1870@HRH-IC0027332.wrdsb.ca>
2016-10-25 13:16:48 -0400

    Work on this at home and catch up

---
+++ src/A4Q8.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gavra1870
+ */
+public class A4Q8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // new scanner 
+        Scanner  input = new Scanner(System.in);
+        double sum = 1;
+        //ask for  sum of dice
+        System.out.println("Enter the sum of the number");
+        double num = input.nextDouble();
+        //if the sum of the dice is zero
+        
+        
+        
+    }
+}
